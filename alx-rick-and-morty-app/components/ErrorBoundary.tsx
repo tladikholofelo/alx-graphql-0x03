0c1f183fd55ae369c60adda8df483d040f9eca8d
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2025-02-02 02:42:14 +0200

    Create components/ErrorBoundary.tsx

---
+++ alx-rick-and-morty-app/components/ErrorBoundary.tsx
@@ -0,0 +1,44 @@
+import React, { ReactNode } from 'react';
+import * as Sentry from '@sentry/react'; // Import Sentry
+
+interface State {
+  hasError: boolean;
+}
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, State> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    // Log the error to Sentry
+    Sentry.captureException(error, { extra: errorInfo });
+    console.log({ error, errorInfo });  // Optional: also log to the console
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h2>Oops, there is an error!</h2>
+          <button onClick={() => this.setState({ hasError: false })}>
+            Try again?
+          </button>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
